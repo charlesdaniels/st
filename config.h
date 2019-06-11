31dc9d21fe89fd850111ba6afe1420472dc9ab05
Charles Daniels <charles@cdaniels.net>
2019-06-11 10:49:15 -0400

    switch to gruvbox light hard contrast

--- config.h
+++ config.h
@@ -89,22 +89,22 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#f9f5d7",		/* gruvbox light hard contrast */
 	"#cc241d",
 	"#98971a",
 	"#d79921",
 	"#458588",
 	"#b16286",
 	"#689d6a",
-	"#a89984",
+	"#7c6f64",
 	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#9d0006",
+	"#79740e",
+	"#b57614",
+	"#076678",
+	"#8f3f71",
+	"#427b58",
+	"#3c3836",
 
 	[255] = 0,
 
