c9d94b7e3e1f2da5fe380605f51e16bcd071425e
Charles Daniels <charles@cdaniels.net>
2020-09-13 07:07:32 -0400

    fix #3

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
-char *vtiden = "\033[?6c";
+char *vtiden = "\033[?12;4c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
